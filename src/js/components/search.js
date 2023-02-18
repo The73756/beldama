1f344a266c8d05fb1182de3c96593237261c49d1
The73756 <the73756the@gmail.com>
2023-02-18 14:38:11 +0900

    finish header without adaptive

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!searchForm) return null;
 
-  const searchBtn = document.querySelector('.search__btn');
+  const searchBtn = searchForm.querySelector('.search__btn');
+  const searchInput = searchForm.querySelector('.search__input');
 
-  searchBtn.addEventListener('click', () => {
+  const toggleButtonState = () => {
     const isActive = searchForm.classList.contains('search--active');
 
     searchForm.classList.toggle('search--active');
     searchBtn.type = isActive ? 'submit' : 'button';
-  });
+  };
+
+  searchBtn.addEventListener('click', toggleButtonState);
+  searchInput.addEventListener('focus', toggleButtonState);
+  searchInput.addEventListener('blur', toggleButtonState);
 
   document.addEventListener('click', (e) => {
     const isActive = searchForm.classList.contains('search--active');
