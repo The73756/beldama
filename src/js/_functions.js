6f803518313f781ebfd1b77b0803eaec2549d5dc
The73756 <the73756the@gmail.com>
2023-02-19 19:28:00 +0900

    fix adaptive and pre-deploy

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -2,8 +2,8 @@
 // Рекомендуется создавать отдельный файл в папке components и подключать все там
 
 // Определение операционной системы на мобильных
-// import { mobileCheck } from "./functions/mobile-check";
-// console.log(mobileCheck())
+import { mobileCheck } from './functions/mobile-check';
+mobileCheck();
 
 // Определение ширины экрана
 // import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
